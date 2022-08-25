39b0e02a449f20fcc6bc7142af717aa7f77b4598
eduard golan <eduard.golan@ebs-integrator.com>
2022-08-25 10:57:51 +0300

    Add square brackets

--- main.tf
+++ main.tf
@@ -44,8 +44,7 @@ resource "azurerm_postgresql_flexible_server" "main" {
   storage_mb = var.storage_size
 
   sku_name   = var.sku_name
-  depends_on = var.azurerm_private_dns_zone_virtual_network_link
-
+  depends_on = [var.azurerm_private_dns_zone_virtual_network_link]
 }
 
 ## ------ Create Postgresl Database ------ ##
